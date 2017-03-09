8f579b5ac1286a6fc464995ac11b1611c188bee6
zysidea <zysidea@gmail.com>
2017-03-09 20:02:12 +0800

    commit Keyboard Row

---
+++ keyboard_row/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	input := []string{"Hello", "Alaska", "Dad", "Peace"}
+	output := findWords(input)
+	for _, v := range output {
+		fmt.Print(v + " ")
+	}
+}
+
+var (
+	firstKey = []string{"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"}
+
+	secondKey = []string{"A", "S", "D", "F", "G", "H", "J", "K", "L", "a", "s", "d", "f", "g", "h", "j", "k", "l"}
+
+	thirdKey = []string{"Z", "X", "C", "V", "B", "N", "M", "z", "x", "c", "v", "b", "n", "m"}
+)
+
+func findWords(words []string) []string {
+	var result []string
+	for _, v1 := range words {
+		if match(v1, firstKey) && !match(v1, secondKey) && !match(v1, thirdKey) {
+			result = append(result, v1)
+		} else if !match(v1, firstKey) && match(v1, secondKey) && !match(v1, thirdKey) {
+			result = append(result, v1)
+		} else if !match(v1, firstKey) && !match(v1, secondKey) && match(v1, thirdKey) {
+			result = append(result, v1)
+		} else {
+			continue
+		}
+	}
+	return result
+}
+
+func match(str string, matchs []string) bool {
+	for _, v := range matchs {
+		if strings.Contains(str, v) {
+			return true
+		}
+	}
+	return false
+}
